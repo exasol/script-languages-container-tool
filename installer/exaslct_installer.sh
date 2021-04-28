07286b89effad6d64fe9477564cf5beaa61e16b6
Torsten Kilias <torsten.kilias@exasol.com>
2021-04-28 17:06:09 +0200

    Fix installer

--- installer/exaslct_installer.sh
+++ installer/exaslct_installer.sh
@@ -73,7 +73,7 @@ main() {
 
   mv exaslct_install_template.sh exaslct.sh
 
-  sed "s/<<<<RUNNER_IMAGE_NAME>>>>/$exaslct_git_ref/g" exaslct.sh
+  sed -i "s/<<<<RUNNER_IMAGE_NAME>>>>/$exaslct_git_ref/g" exaslct.sh
 
   popd
 
