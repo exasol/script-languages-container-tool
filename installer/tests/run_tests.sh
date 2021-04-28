ae2c217f73ab354c7b374e19920f69558952fefe
Torsten Kilias <torsten.kilias@exasol.com>
2021-04-28 18:19:07 +0200

    Improve installer/tests/run_tests.sh and cleanup bash -x

--- installer/tests/run_tests.sh
+++ installer/tests/run_tests.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -u
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 echo "Running test_exaslct_install_template_with_current_ref.sh"
 bash "$SCRIPT_DIR/test_exaslct_install_template_with_current_ref.sh"
+echo
+echo "====================================================================="
+echo
 
 echo "Running test_install_or_update_exaslct_with_current_ref_default_environment.sh"
 bash "$SCRIPT_DIR/test_install_or_update_exaslct_with_current_ref_default_environment.sh"
-
-
+echo
+echo "====================================================================="
+echo
