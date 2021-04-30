3ecd772f31e24514e32fc922d4231ceed8c1a896
Torsten Kilias <torsten.kilias@exasol.com>
2021-04-30 11:10:23 +0200

    Fix default for install_or_update_exaslct.sh
    - build_docker_runner_image.sh now returns the image name at stdout
    - push_docker_runner_image.sh can get a parameter to define the suffix of the image name
    - push_docker_runner_image.sh now builds the image before pushing

--- starter_scripts/build_docker_runner_image.sh
+++ starter_scripts/build_docker_runner_image.sh
@@ -7,4 +7,6 @@ PROJECT_ROOT_DIR="$SCRIPT_DIR/.."
 
 IMAGE_NAME="$("$SCRIPT_DIR/construct_docker_runner_image_name.sh")"
 
-docker build -t "$IMAGE_NAME" -f "$SCRIPT_DIR/Dockerfile" "$PROJECT_ROOT_DIR"
+docker build -t "$IMAGE_NAME" -f "$SCRIPT_DIR/Dockerfile" "$PROJECT_ROOT_DIR" 1>&2
+
+echo "$IMAGE_NAME"
--- starter_scripts/push_docker_runner_image.sh
+++ starter_scripts/push_docker_runner_image.sh
@@ -2,14 +2,20 @@
 
 set -euo pipefail
 
+if [ -n "${1-}" ]; then
+  image_suffix="$1"
+else
+  image_suffix="latest"
+fi
+
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-GIT_IMAGE_NAME="$("$SCRIPT_DIR/construct_docker_runner_image_name.sh")"
+GIT_IMAGE_NAME="$("$SCRIPT_DIR/build_docker_runner_image.sh")"
 
 docker push "$GIT_IMAGE_NAME"
 
-LATEST_IMAGE_NAME="$("$SCRIPT_DIR/construct_docker_runner_image_name.sh" latest)"
+RENAMED_IMAGE_NAME="$("$SCRIPT_DIR/construct_docker_runner_image_name.sh" "$image_suffix")"
 
-docker tag "$GIT_IMAGE_NAME" "$LATEST_IMAGE_NAME"
+docker tag "$GIT_IMAGE_NAME" "$RENAMED_IMAGE_NAME"
 
-docker push "$LATEST_IMAGE_NAME"
+docker push "$RENAMED_IMAGE_NAME"
