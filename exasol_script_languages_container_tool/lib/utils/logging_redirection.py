c40ef83e9f38d7fe2cf7060ae098e14192f43117
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-05-28 13:03:47 -0300

    Add tee for writing to file and console.

--- exasol_script_languages_container_tool/lib/utils/logging_redirection.py
+++ exasol_script_languages_container_tool/lib/utils/logging_redirection.py
@@ -1,6 +1,31 @@
 import sys
 
 
+class _Tee(object):
+    # Helper object which redirects calls to stdout/stderr to itself, prints to a give file and to real stdout/stderr
+    def __init__(self, name, mode):
+        self.file = open(name, mode)
+        self.stdout = sys.stdout
+        self.stderr = sys.stderr
+        sys.stdout = self
+        sys.stderr = self
+
+    def get_file(self):
+        return self.file
+
+    def __del__(self):
+        sys.stdout = self.stdout
+        sys.stderr = self.stderr
+        self.file.close()
+
+    def write(self, data):
+        self.file.write(data)
+        self.stdout.write(data)
+
+    def flush(self):
+        self.file.flush()
+
+
 def create_docker_build_creator(task_creator):
     # Provides redirection of ALL logs to exaslct.log in the tasks job directory
 
@@ -8,10 +33,8 @@ def create_docker_build_creator(task_creator):
     #   task_creator (function): creator function of the task
     def create_docker_build():
         task = task_creator()
-        logging_redirection_file = open(f'{task.get_log_path()}/exaslct.log', "w")
-        print(f'Logging to :{logging_redirection_file.name}')
-        sys.stdout = logging_redirection_file
-        sys.stderr = logging_redirection_file
+        tee = _Tee(f'{task.get_log_path()}/exaslct.log', "w")
+        print(f'Logging to :{tee.get_file().name}')
         return task
 
     return create_docker_build
