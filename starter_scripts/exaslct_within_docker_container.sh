a7845d6b8d08134e80750abeb8b1c772c9f8dd23
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-06-01 15:53:08 -0300

    Use flat list of relevant mount point arguments.

--- starter_scripts/exaslct_within_docker_container.sh
+++ starter_scripts/exaslct_within_docker_container.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#set -e => immediately exit if any command [1] has a non-zero exit status
+#set -u => reference to any variable you haven't previously defined is an error and causes the program to immediately exit.
+#set -o pipefailt => This setting prevents errors in a pipeline from being masked.
+#                    If any command in a pipeline fails,
+#                    that return code will be used as the return code of the whole pipeline.
 set -euo pipefail
 
 RUNNER_IMAGE_NAME="$1"
@@ -12,60 +17,45 @@ else
 fi
 
 
+relevant_mount_point_arguments=("--flavor-path" "--export-path" "--output-directory" "--temporary-base-directory"
+                                "--cache-directory" "--save-directory" "--task-dependencies-dot-file" "--test-folder"
+                                "--test-file" "--temporary-base-directory")
 
-build_mount_point_arguments=("--flavor-path" "--output-directory" "--temporary-base-directory"
-                                "--cache-directory" "--task-dependencies-dot-file")
-build_test_container_mount_point_arguments=("--temporary-base-directory" "--task-dependencies-dot-file")
-clean_all_images_mount_point_arguments=("--output-directory" "--task-dependencies-dot-file")
-clean_flavor_images_mount_point_arguments=("--flavor-path", "--output-directory", "--task-dependencies-dot-file")
-export_mount_point_arguments=("--flavor-path" "--export-path " "--output-directory"
-                                " --temporary-base-directory" "--cache-directory" "--task-dependencies-dot-file")
-generate_language_activation_mount_point_arguments=("--flavor-path")
-push_mount_point_arguments=("--flavor-path" "--output-directory" "--temporary-base-directory"
-                              "--cache-directory" "--task-dependencies-dot-file")
-push_test_container_mount_point_arguments=("--output-directory" "--temporary-base-directory"
-                                "--cache-directory" "--task-dependencies-dot-file")
-run_db_test_mount_point_arguments=("--flavor-path" " --test-folder" "--test-file"
-                                      "--output-directory" "--temporary-base-directory"
-                                      "--cache-directory" "--task-dependencies-dot-file")
-save_mount_point_arguments=("--save-directory" "--flavor-path"  "--output-directory" "--temporary-base-directory"
-                                      "--cache-directory" "--task-dependencies-dot-file")
-spawn_test_environment_mount_point_arguments=("--output-directory" "--temporary-base-directory"
-                                "--task-dependencies-dot-file")
-upload_mount_point_arguments=("--flavor-path" "--output-directory" "--temporary-base-directory"
-                                "--cache-directory" "--task-dependencies-dot-file")
+function _get_mount_point_argument() {
+  current_arg=$1
+  next_arg=$2
+#  temp="${next_arg%\"}" NEED TO CHECK IF I NEED ARGUMENTS WITH/WITHOUT QUOTES
+#  temp="${temp#\"}"
+  if [ -d  $next_arg ]; then
+    mount_point_arguments+=$next_arg
+  elif [ -f $next_arg ]; then
+    mount_point_arguments+="$(dirname "${next_arg}")"
+  else
+    echo "Invalid argument for $current_arg: $next_arg needs to be a directory"
+    exit -1
+  fi
+}
 
+function _get_mount_point_arguments() {
+  lenArgs="$#"
+  retVal=()
+  echo "Len Args: $lenArgs"
+  for ((idxArg=1; idxArg < $lenArgs; idxArg++))
+    do
+    current_arg=${!idxArg}
+    next_arg_idx=$((idxArg+1))
+    next_arg=${!next_arg_idx}
+    echo "Check $current_arg"
+    if [[ " ${relevant_mount_point_arguments[@]} " =~ " ${current_arg} " ]]; then
+      echo "Found:$next_arg"
+      _get_mount_point_argument $current_arg $next_arg
+    fi
+    done
+}
 
-
-declare -A cmd_related_mount_points
-cmd_related_mount_points=( ["build"]=${build_mount_point_arguments[@]}
-                                      ["build-test-container"]=${build_test_container_mount_point_arguments[@]}
-                                      ["clean-all-images"]=${clean_all_images_mount_point_arguments[@]}
-                                      ["clean-flavor-images"]=${clean_flavor_images_mount_point_arguments[@]}
-                                      ["export"]=${export_mount_point_arguments[@]}
-                                      ["generate-language-activation"]=${generate_language_activation_mount_point_arguments[@]}
-                                      ["push"]=${push_mount_point_arguments[@]}
-                                      ["push-test-container"]=${push_test_container_mount_point_arguments[@]}
-                                      ["run-db-test"]=${run_db_test_mount_point_arguments[@]}
-                                      ["save"]=${save_mount_point_arguments[@]}
-                                      ["spawn-test-environment"]=${spawn_test_environment_mount_point_arguments[@]}
-                                      ["upload"]=${upload_mount_point_arguments[@]})
-
-echo "${cmd_related_mount_points['build']}"
-
-base_cmd=$1
-
-echo "base command: $base_cmd"
-
-valid_mount_point_arguments=''
-if [ -v cmd_related_mount_points[$base_cmd] ] ; then
-  valid_mount_point_arguments=cmd_related_mount_points[$base_cmd]
-  echo "Valid arguments:${cmd_related_mount_points[$base_cmd]}"
-else
-  echo "Invalid command"
-  exit -1
-fi
-
+mount_point_arguments=''
+_get_mount_point_arguments ${@}
+echo "MountPoint args:$mount_point_arguments"
 
 
 quoted_arguments=''
@@ -114,6 +104,6 @@ trap 'rm -f -- "$tmpfile_env"' INT TERM HUP EXIT
 
 create_env_file_debug_protected "$tmpfile_env"
 
-docker run --env-file "$tmpfile_env" --rm $terminal_parameter -v "$PWD:$PWD" -v "$DOCKER_SOCKET_MOUNT" -w "$PWD" "$RUNNER_IMAGE_NAME" bash -c "$RUN_COMMAND"
+docker run --env-file "$tmpfile_env" --rm $terminal_parameter -v "$PWD:$PWD" -v "$DOCKER_SOCKET_MOUNT" "$mount_point_arguments" -w "$PWD" "$RUNNER_IMAGE_NAME" bash -c "$RUN_COMMAND"
 
 umask "$old_umask"
