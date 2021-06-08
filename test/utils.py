fdd574433ba7bd27397844e25b450f06edfe3e7c
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-06-08 16:36:10 -0300

    #26:Using host network for execution of exasclt
    In case of exasclt upload the user might try to push image to a custom network path. For that we need to execute the exasclt docker instance in the host network, not the isolated docker network.
    
    Also, execute tests within container, by using exaslct script instead of starter_scripts/exaslct_without_poetry.sh

--- test/utils.py
+++ test/utils.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-EXASLCT_DEFAULT_BIN = Path(Path(__file__).parent.parent, "starter_scripts/exaslct_without_poetry.sh")
+EXASLCT_DEFAULT_BIN = Path(Path(__file__).parent.parent, "exaslct")
