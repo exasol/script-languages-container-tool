1fa3056aa01759a2e0fcd78fd1f2c433c41851b4
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-07-02 07:30:01 -0300

    Support for more Linux distributions.

--- ci_tests/get_ci_envs.sh
+++ ci_tests/get_ci_envs.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-CI_ENVS=("CentOs7")
+CI_ENVS=("CentOs7" "CentOs8" "Ubuntu18.04" "Ubuntu20.04")
 printf '%s\n' ${CI_ENVS[@]} | jq -R . | jq -cs .
--- ci_tests/run_ci_test.sh
+++ ci_tests/run_ci_test.sh
@@ -12,7 +12,7 @@ CI_ENV="$1"
 IMAGE_NAME="script-languages-container-test-env:${CI_ENV}"
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-docker build -t "$IMAGE_NAME" "$SCRIPT_DIR/$CI_ENV"
+docker build -t "$IMAGE_NAME" -f "$SCRIPT_DIR/$CI_ENV/Dockerfile" "$SCRIPT_DIR"
 docker container rm -f -v ci_env_test || true
 #Need to mount docker socket as we use docker within the CI environment
 #Also need to mount root directory of script-languages-container-tool because we want to test this commit
