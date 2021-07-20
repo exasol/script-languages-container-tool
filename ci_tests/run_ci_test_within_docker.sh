279e7e90100949c34980d9ffad8ec5ef3f3fcaed
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-07-20 16:16:29 -0300

    Fix installer
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Tue Jul 20 16:16:29 2021 -0300
    #
    # On branch bug/#52_fix_mount_binding
    # Changes to be committed:
    #	modified:   ci_tests/run_ci_test_within_docker.sh
    #	modified:   installer/checksums/exaslct_installer.sh.sha512sum
    #	modified:   installer/exaslct_installer.sh
    #
    # Untracked files:
    #	DockerUploadTest.dot
    #	Pipfile
    #	default
    #	~/
    #
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Tue Jul 20 16:16:29 2021 -0300
    #
    # On branch bug/#52_fix_mount_binding
    # Changes to be committed:
    #	modified:   ci_tests/run_ci_test_within_docker.sh
    #	modified:   installer/checksums/exaslct_installer.sh.sha512sum
    #	modified:   installer/exaslct_installer.sh
    #	modified:   starter_scripts/checksums/exaslct_within_docker_container.sh.sha512sum
    #
    # Untracked files:
    #	DockerUploadTest.dot
    #	Pipfile
    #	default
    #	~/
    #

--- ci_tests/run_ci_test_within_docker.sh
+++ ci_tests/run_ci_test_within_docker.sh
@@ -4,8 +4,8 @@ set -e
 
 cd $1
 
-echo ************* RUN BASH TEST **********
+echo "************* RUN BASH TEST **********"
 ./starter_scripts/test/test_scripts.sh
-echo ************* FINISHED BASH TEST **********
+echo "************* FINISHED BASH TEST *****"
 
 ./exaslct export --flavor-path ./test/resources/test-flavor --export-path ./out
