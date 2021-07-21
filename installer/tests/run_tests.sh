40392b7bfbce2a0a0a3a1b775c82e4d86dcd5dce
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-07-21 11:20:49 -0300

    installer/tests/run_tests.sh: Accumulate error variable and return; but don't break when single test fails. This allows execution of all tests, but recognizing error case.

--- installer/tests/run_tests.sh
+++ installer/tests/run_tests.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-#set -e => immediately exit if any command [1] has a non-zero exit status
-#set -u => reference to any variable you haven't previously defined is an error and causes the program to immediately exit.
-#set -o pipefail => This setting prevents errors in a pipeline from being masked.
-#                    If any command in a pipeline fails,
-#                    that return code will be used as the return code of the whole pipeline.
-set -euo pipefail
+set -u
 
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
+returnValue=0
+
 echo "Running test_exaslct_install_template_with_current_ref.sh"
 bash "$SCRIPT_DIR/test_exaslct_install_template_with_current_ref.sh"
+(( returnValue+=$? ))
 echo
 echo "====================================================================="
 echo
 
 echo "Running test_install_or_update_exaslct_with_current_ref_default_environment.sh"
 bash "$SCRIPT_DIR/test_install_or_update_exaslct_with_current_ref_default_environment.sh"
+(( returnValue+=$? ))
 echo
 echo "====================================================================="
 echo
+exit $returnValue
