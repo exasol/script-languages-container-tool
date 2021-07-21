daa8dbcf0c1b4f6859a5c2727fb9db2596c79e3f
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-07-21 07:10:22 -0300

    Make installer test script fail if any subcommands fail

--- installer/tests/run_tests.sh
+++ installer/tests/run_tests.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-set -u
+#set -e => immediately exit if any command [1] has a non-zero exit status
+#set -u => reference to any variable you haven't previously defined is an error and causes the program to immediately exit.
+#set -o pipefail => This setting prevents errors in a pipeline from being masked.
+#                    If any command in a pipeline fails,
+#                    that return code will be used as the return code of the whole pipeline.
+set -euo pipefail
+
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
