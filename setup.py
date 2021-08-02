fdc842af1fbd5c796333b10490596307feba763c
Torsten Kilias <torsten.kilias@exasol.com>
2021-08-02 16:52:03 +0200

    Update integration-test-docker-environment to version 0.4.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ package_data = \
 
 install_requires = \
 ['exasol-integration-test-docker-environment @ '
- 'git+https://github.com/exasol/integration-test-docker-environment.git@0.4.0']
+ 'git+https://github.com/exasol/integration-test-docker-environment.git@0.4.1']
 
 setup_kwargs = {
     'name': 'exasol-script-languages-container-tool',
