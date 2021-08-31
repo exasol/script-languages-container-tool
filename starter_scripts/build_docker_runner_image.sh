f156b5e0934a1ea260b321e124c1f6b618846e12
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-08-31 06:36:09 -0300

    #69: Created slim version of exaslct for MacOsX (#71)
    Created a slim version of exaslct which is compatible with restrictions on MacOsX:
    1. Using greadlink instead of readlink
    2. Not using any newer bash features (associative arrays) due to very old bash version on MacOsX. This prohibits the parsing of the mount binding directories, and restricts the usage of exaslct to directories within the current directory.

--- starter_scripts/build_docker_runner_image.sh
+++ starter_scripts/build_docker_runner_image.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+#####################################################################################
+###REMEMBER TO TEST ANY CHANGES HERE ON MACOSX!!!
+#####################################################################################
+
 set -euo pipefail
 
-SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+rl=readlink
+if [[ "$(uname)" = Darwin ]]; then
+  rl=greadlink
+fi
+
+
+if [[ ! "$(command -v $rl)" ]]; then
+  echo readlink not available! Please install coreutils: On Linux \"apt-get install coreutils\" or similar. On MacOsX \"brew install coreutils\".
+  exit 1
+fi
+
+SCRIPT_DIR="$(dirname "$($rl -f "${BASH_SOURCE[0]}")")"
+
 PROJECT_ROOT_DIR="$SCRIPT_DIR/.."
 
 IMAGE_NAME="$("$SCRIPT_DIR/construct_docker_runner_image_name.sh")"
