9e08f15cd0a6ecdf27ff012817ec52272b10a7c0
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-10-25 10:40:35 -0300

    Set lang-path parameter.

--- exasol_script_languages_container_tool/lib/tasks/test/run_db_test.py
+++ exasol_script_languages_container_tool/lib/tasks/test/run_db_test.py
@@ -84,6 +84,7 @@ class RunDBTest(FlavorBaseTask,
         environment = "--driver=/downloads/ODBC/lib/linux/x86_64/libexaodbc-uo2214lv2.so  " \
                       "--jdbc-path /downloads/JDBC/exajdbc.jar"
         language_definition = f"--script-languages '{self.language_definition}'"
+        language_path = f"--lang-path /tests/lang"
         language = ""
         if self.language is not None:
             language = "--lang %s" % self.language
@@ -93,6 +94,7 @@ class RunDBTest(FlavorBaseTask,
                          server,
                          credentials,
                          language_definition,
+                         language_path,
                          log_level,
                          environment,
                          language,
