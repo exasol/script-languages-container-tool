77f20e0f735b7b08d4fa048479cee91b9b905cae
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-10-26 15:13:27 -0300

    Fixed tests.

--- tests/test/docker_environment_test.py
+++ tests/test/docker_environment_test.py
@@ -1,24 +1,19 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
-import os
-import sys
-
-sys.path.append(os.path.realpath(__file__ + '/../../lib'))
-
-import udf
-import docker_db_environment
+from exasol_python_test_framework import udf
+from exasol_python_test_framework import docker_db_environment
 
 
 class DockerDBEnvironmentTest(udf.TestCase):
 
     @udf.skipIfNot(docker_db_environment.is_available, reason="This test requires a docker-db environment")
     def test_connect_from_udf_to_other_container(self):
-        schema="test_connect_from_udf_to_other_container"
-        env=docker_db_environment.DockerDBEnvironment(schema)
+        schema = "test_connect_from_udf_to_other_container"
+        env = docker_db_environment.DockerDBEnvironment(schema)
         try:
-            self.query(udf.fixindent("DROP SCHEMA %s CASCADE"%schema),ignore_errors=True)
-            self.query(udf.fixindent("CREATE SCHEMA %s"%schema))
-            self.query(udf.fixindent("OPEN SCHEMA %s"%schema))
+            self.query(udf.fixindent("DROP SCHEMA %s CASCADE" % schema), ignore_errors=True)
+            self.query(udf.fixindent("CREATE SCHEMA %s" % schema))
+            self.query(udf.fixindent("OPEN SCHEMA %s" % schema))
             self.query(udf.fixindent('''
                 CREATE OR REPLACE PYTHON SCALAR SCRIPT connect_container(host varchar(1000), port int)  returns int AS
                 import socket
@@ -28,13 +23,13 @@ class DockerDBEnvironmentTest(udf.TestCase):
                     return 0
                 /
                 '''))
-            container=env.run(name="netcat",image="busybox:1",command="nc -v -l -s 0.0.0.0 -p 7777",)
-            host=env.get_ip_address_of_container(container)
-            self.query("select connect_container('%s',%s)"%(host,7777))
+            container = env.run(name="netcat", image="busybox:1", command="nc -v -l -s 0.0.0.0 -p 7777", )
+            host = env.get_ip_address_of_container(container)
+            self.query("select connect_container('%s',%s)" % (host, 7777))
             self.assertTrue("connect" in container.logs())
         finally:
             try:
-                self.query(udf.fixindent("DROP SCHEMA %s CASCADE"%schema))
+                self.query(udf.fixindent("DROP SCHEMA %s CASCADE" % schema))
             except:
                 pass
             try:
@@ -42,6 +37,6 @@ class DockerDBEnvironmentTest(udf.TestCase):
             except:
                 pass
 
+
 if __name__ == '__main__':
     udf.main()
-
