567a0a7e613aa9dfbd4d8ee61d9273009f460bad
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-11-17 11:26:21 -0300

    Added upload of release artifacts

---
+++ scripts/build/build_release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+   
+
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" 
+ 
+source "$SCRIPT_DIR/../../starter_scripts/poetry_utils.sh"
+
+check_requirements
+
+set -euo pipefail
+
+init_poetry
+
+if [ -n "$POETRY_BIN" ]
+then
+  PYTHONPATH=. $POETRY_BIN build
+else
+  echo "Could not find poetry!"
+  exit 1
+fi
+
