55ee1aa1e457165ed4384dd0d77e962162e885a3
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-12-02 11:44:33 -0300

    1. Download report from container instead of using mount binding

--- exasol_script_languages_container_tool/lib/tasks/security_scan/security_scan.py
+++ exasol_script_languages_container_tool/lib/tasks/security_scan/security_scan.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from typing import Dict
 
 import luigi
-from docker.types import Mount
+
+import tarfile
 
 from exasol_integration_test_docker_environment.lib.base.flavor_task import FlavorsBaseTask
 from exasol_integration_test_docker_environment.lib.config.build_config import build_config
@@ -12,9 +13,11 @@ from exasol_script_languages_container_tool.lib.tasks.build.docker_flavor_build_
 
 from exasol_script_languages_container_tool.lib.tasks.security_scan.security_scan_parameter import SecurityScanParameter
 
+from docker.models.containers import Container
+
 
 class ScanResult:
-    def __init__(self, is_ok, summary, report_dir):
+    def __init__(self, is_ok: bool, summary: str, report_dir: Path):
         self.is_ok = is_ok
         self.summary = summary
         self.report_dir = report_dir
@@ -75,28 +78,37 @@ class SecurityScanner(DockerFlavorBuildBase, SecurityScanParameter):
         flavor_path = Path(self.flavor_path)
         report_path = self.report_path.joinpath(flavor_path.name)
         report_path.mkdir(parents=True, exist_ok=True)
-        report_path_abs = str(report_path.absolute())
+        report_path_abs = report_path.absolute()
         result = ScanResult(is_ok=False, summary="", report_dir=report_path_abs)
         assert len(task_results.values()) == 1
         for task_result in task_results.values():
             self.logger.info(f"Running security run on image:{task_result.get_target_complete_name()}, report path: "
                              f"{report_path_abs}")
 
+            report_local_path = "/report"
             with ContextDockerClient() as docker_client:
-                mounts = [Mount(source=report_path_abs, target=report_path_abs, type="bind")]
                 result_container = docker_client.containers.run(task_result.get_target_complete_name(),
-                                                                command=report_path_abs, mounts=mounts,
+                                                                command=report_local_path,
                                                                 detach=True, stderr=True)
                 try:
                     logs = result_container.logs(follow=True).decode("UTF-8")
                     result_container_result = result_container.wait()
-
+                    self._write_report(result_container, report_path_abs, report_local_path)
                     result = ScanResult(is_ok=(result_container_result["StatusCode"] == 0),
                                         summary=logs, report_dir=report_path_abs)
                 finally:
                     result_container.remove()
 
         self.return_object(result)
+
+    def _write_report(self, container: Container, report_path_abs: Path, report_local_path: str):
+        tar_file_path = report_path_abs / 'report.tar'
+        with open(tar_file_path, 'wb') as tar_file:
+            bits, stat = container.get_archive(report_local_path)
+            for chunk in bits:
+                tar_file.write(chunk)
+        with tarfile.open(tar_file_path) as tar_file:
+            tar_file.extractall(path=report_path_abs)
 
 
 class AllScanResult:
