d7da6e28745187cc74f463d192d54e1b2394d84c
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-12-30 11:04:37 -0300

    1. Added ignore tag to get_all_tests.sh

--- scripts/test/get_all_tests.sh
+++ scripts/test/get_all_tests.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-# shellcheck disable=SC2207
-declare -a test_array=( $(ls "$SCRIPT_DIR/../../test/test_*.py"))
+# shellcheck disable=SC2207,SC2086
+declare -a test_array=( $(ls $SCRIPT_DIR/../../test/test_*.py))
 printf '%s\n' "${test_array[@]}" | jq -R . | jq -cs .
