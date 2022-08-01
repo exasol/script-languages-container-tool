8bb5ae3e3f7bac1d28aa6107ab3ba83bb6b611aa
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-08-01 14:37:53 -0300

    Fixed get_all_tests.sh

--- scripts/test/get_all_tests.sh
+++ scripts/test/get_all_tests.sh
@@ -6,7 +6,8 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 # Ignore shellcheck rule SC2086 here: ls does not work correct with quotes here (probably because of the asterisk).
 # shellcheck disable=SC2207,SC2086
 declare -a test_array
-for test_file in $SCRIPT_DIR/../../test/test_*.py; do
-  test_array=("${test_array[@]}" $test_file $(basename "$test_file"))
+for test_file in "$SCRIPT_DIR"/../../test/test_*.py; do
+  test_name="$(basename "$test_file")"
+  test_array=("${test_array[@]}" "$test_file" "$test_name")
 done
 printf '{"path": "%s", "name": "%s"}\n' "${test_array[@]}" | jq -cs .
