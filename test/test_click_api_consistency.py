0a2a3440e86aeed1f8f147cf25880e80589bd58c
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-08-10 19:30:38 -0300

    adjusted test_click_api_consistency.py

--- test/test_click_api_consistency.py
+++ test/test_click_api_consistency.py
@@ -1,7 +1,7 @@
 import unittest
-from typing import List, Any, Tuple
 
-import click.types
+from exasol_integration_test_docker_environment.testing.api_consistency_utils import get_click_and_api_function_names, \
+    defaults_of_click_call, get_click_and_api_functions, param_names_of_click_call
 
 from exasol_script_languages_container_tool.cli import commands
 from exasol_script_languages_container_tool.lib import api
@@ -10,36 +10,8 @@ import inspect
 from exasol_integration_test_docker_environment.testing.utils import multiassert
 
 
-def is_click_command(obj: Any) -> bool:
-    return isinstance(obj, click.Command)
-
-
 class ClickApiConsistency(unittest.TestCase):
 
-    @staticmethod
-    def _adjust_default_value_for_multiple(x: Any):
-        """
-        Click stores default values as list if 'Multiple'=true. However, for plain Python methods we
-        need to use (immutable) tuples for declaring default value. Hence, we need to convert lists with tuples
-        for comparison.
-        Also, default values for none-required parameters of with multiple=true are marked as "None"
-        in the command object, but in reality click invokes the command with "tuple()" for that parameter.
-        (Note that we use here internal structures of click and don't have guarantee of the behavior)
-        """
-        def_value = x.default
-        if x.multiple:
-            if type(def_value) == list:
-                return tuple(def_value)
-            elif def_value is None:
-                return tuple()
-        return def_value
-
-    def _defaults_of_click_call(self, click_call: click.Command) -> List[Tuple[str, Any]]:
-        return [(o.name, self._adjust_default_value_for_multiple(o)) for o in click_call.params if not o.required]
-
-    @staticmethod
-    def _param_names_of_click_call(click_call: click.Command) -> List[str]:
-        return [o.name for o in click_call.params]
 
     def test_api_arguments(self):
         """
@@ -47,38 +19,34 @@ class ClickApiConsistency(unittest.TestCase):
         """
         self.maxDiff = None
 
-        # Get all click commands in module exasol_script_languages_container_tool.cli.commands
-        click_commands = [c[1] for c in inspect.getmembers(commands, is_click_command)]
-        # Get all functions in module exasol_script_languages_container_tool.lib.api
-        api_functions = [f[1] for f in inspect.getmembers(api, inspect.isfunction)]
-
+        click_commands, api_functions = get_click_and_api_functions(commands, api)
         # Now iterate over the list and compare consistency
         for cli_call, api_call in zip(click_commands, api_functions):
             cli_spec = inspect.getfullargspec(cli_call.callback)
             api_spec = inspect.getfullargspec(api_call)
 
+            # We don't compare the annotation for the return type as this is allowed to be different between CLI and API
+            if "return" in api_spec.annotations:
+                del api_spec.annotations["return"]
+
             multiassert([lambda: self.assertEqual(api_spec.args, cli_spec.args),
                          lambda: self.assertEqual(api_spec.annotations, cli_spec.annotations),
-                         lambda: self.assertEqual(api_spec.args,
-                                                  self._param_names_of_click_call(cli_call))], self)
+                         lambda: self.assertEqual(api_spec.args, param_names_of_click_call(cli_call))], self)
 
     def test_api_default_values(self):
         """
         Validate that the default values for all commands match!
         """
 
-        # Get all click commands in module exasol_script_languages_container_tool.cli.commands
-        click_commands = [c[1] for c in inspect.getmembers(commands, is_click_command)]
-        # Get all functions in module exasol_script_languages_container_tool.lib.api
-        api_functions = [f[1] for f in inspect.getmembers(api, inspect.isfunction)]
+        click_commands, api_functions = get_click_and_api_functions(commands, api)
 
         # Now iterate over the list and compare consistency
         for cli_call, api_call in zip(click_commands, api_functions):
-            api_spec = inspect.getfullargspec(api_call)
-            cli_defaults = self._defaults_of_click_call(cli_call)
+            api_spec_defaults = inspect.getfullargspec(api_call).defaults or tuple()
+            cli_defaults = defaults_of_click_call(cli_call)
 
-            self.assertEqual(len(cli_defaults), len(api_spec.defaults))
-            for api_default_value, cli_default in zip(api_spec.defaults, cli_defaults):
+            self.assertEqual(len(cli_defaults), len(api_spec_defaults))
+            for api_default_value, cli_default in zip(api_spec_defaults, cli_defaults):
                 cli_param_name, cli_default_value = cli_default
                 if api_default_value != cli_default_value:
                     self.fail(f"Default value for parameter '{cli_param_name}' "
@@ -92,10 +60,7 @@ class ClickApiConsistency(unittest.TestCase):
         For that we use inspect to get all classes of type click.Command in module 'commands',
         and on the other hand get all functions in module 'api'. The list of names from both most be identical.
         """
-        click_commands = inspect.getmembers(commands, is_click_command)
-        click_command_names = [c[0] for c in click_commands]
-        api_functions = inspect.getmembers(api, inspect.isfunction)
-        api_function_names = [a[0] for a in api_functions]
+        click_command_names, api_function_names = get_click_and_api_function_names(commands, api)
         self.assertEqual(click_command_names, api_function_names)
 
 
