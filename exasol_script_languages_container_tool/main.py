fe12cc764f4ff316b45adf47dc4092f957d1f5c2
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-08-11 09:59:04 -0300

    fixed main.py

--- exasol_script_languages_container_tool/main.py
+++ exasol_script_languages_container_tool/main.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
 from exasol_integration_test_docker_environment.cli.cli import cli
+import exasol_integration_test_docker_environment.cli.commands
 import exasol_script_languages_container_tool.cli.commands
+
 
 if __name__ == '__main__':
     cli()
