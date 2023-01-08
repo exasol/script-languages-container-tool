ba76d1adea6139dff5227a6a9a3fb171065f197d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-08 20:35:08 +0000

    Adding tarfile member sanitization to extractall()

--- exasol_script_languages_container_tool/lib/tasks/security_scan/security_scan.py
+++ exasol_script_languages_container_tool/lib/tasks/security_scan/security_scan.py
@@ -108,7 +108,29 @@ class SecurityScanner(DockerFlavorBuildBase, SecurityScanParameter):
             for chunk in bits:
                 tar_file.write(chunk)
         with tarfile.open(tar_file_path) as tar_file:
-            tar_file.extractall(path=report_path_abs)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar_file, path=report_path_abs)
 
 
 class AllScanResult(Info):
