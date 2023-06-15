2a0f91a639d20a1902277dd622b323709fb3e681
Torsten Kilias <torsten.kilias@exasol.com>
2023-06-15 17:19:30 +0200

    Implement ExaslctTestEnvironmentWithCleanUp as wrapper for ExaslctTestEnvironment of the ITDE, because we will remove get_test_flavor in the future from ExaslctTestEnvironment

--- test/test_docker_api_push.py
+++ test/test_docker_api_push.py
@@ -1,12 +1,9 @@
-import json
 import unittest
 
-
+from exasol_integration_test_docker_environment.testing import utils
 from exasol_integration_test_docker_environment.testing.docker_registry import LocalDockerRegistryContextManager
 
 import utils as exaslct_utils
-from exasol_integration_test_docker_environment.testing import utils
-
 from exasol_script_languages_container_tool.lib import api
 
 
@@ -23,14 +20,14 @@ class ApiDockerPushTest(unittest.TestCase):
     def test_docker_push(self):
         with LocalDockerRegistryContextManager(self.test_environment.name) as local_registry:
             self.test_environment.docker_repository_name = local_registry.name
-            image_infos = api.push(flavor_path=(str(self.test_environment.get_test_flavor()),),
+            image_infos = api.push(flavor_path=(str(exaslct_utils.get_test_flavor()),),
                                    source_docker_repository_name=self.test_environment.docker_repository_name,
                                    target_docker_repository_name=self.test_environment.docker_repository_name)
             print("repos:", local_registry.repositories)
-            self.assertIn(str(self.test_environment.get_test_flavor()), image_infos)
+            self.assertIn(str(exaslct_utils.get_test_flavor()), image_infos)
             images = local_registry.images
             print("images", images)
-            images_info_list = image_infos[str(self.test_environment.get_test_flavor())]
+            images_info_list = image_infos[str(exaslct_utils.get_test_flavor())]
             images_info_list_tags = list(set(image_info.target_tag for image_info in images_info_list)).sort()
             self.assertEqual(images["tags"].sort(), images_info_list_tags,
                              f"{images} doesn't have the expected tags, it only has {len(images['tags'])}")
--- test/utils.py
+++ test/utils.py
@@ -1,10 +1,15 @@
 from pathlib import Path
+from subprocess import CompletedProcess
+from typing import List
 
 from exasol_integration_test_docker_environment.testing import api_test_environment, exaslct_test_environment
+from exasol_integration_test_docker_environment.testing.spawned_test_environments import SpawnedTestEnvironments
 
 from exasol_script_languages_container_tool.lib import api
 
-TEST_CONTAINER_ROOT_DIRECTORY = Path(__file__).parent / "resources" / "test_container"
+RESOURCES_DIRECTORY = Path(__file__).parent / "resources"
+TEST_CONTAINER_ROOT_DIRECTORY = RESOURCES_DIRECTORY / "test_container"
+FLAVORS_ROOT_DIRECTORY = RESOURCES_DIRECTORY / "flavors"
 EXASLCT_DEFAULT_BIN = Path(Path(__file__).parent.parent, "exaslct")
 
 
@@ -26,15 +31,87 @@ class ExaslctApiTestEnvironmentWithCleanup(api_test_environment.ApiTestEnvironme
         api.clean_all_images(docker_repository_name=self.docker_repository_name)
 
 
-class ExaslctTestEnvironmentWithCleanUp(exaslct_test_environment.ExaslctTestEnvironment):
+class ExaslctTestEnvironmentWithCleanUp():
+
+    def __init__(self,
+                 test_object,
+                 executable=EXASLCT_DEFAULT_BIN,
+                 clean_images_at_close=True,
+                 name=None,
+                 flavor_path: Path = FLAVORS_ROOT_DIRECTORY / "test-flavor"):
+        self._flavor_path = flavor_path
+        self._clean_images_at_close = clean_images_at_close
+        self._itde_cli_test_environment = exaslct_test_environment.ExaslctTestEnvironment(
+            test_object=test_object,
+            executable=executable,
+            clean_images_at_close=clean_images_at_close,
+            name=name
+        )
+
+    @property
+    def flavor_path(self):
+        return self._flavor_path
+
+    @property
+    def executable(self):
+        return self._itde_cli_test_environment.executable
+
+    @property
+    def test_object(self):
+        return self._itde_cli_test_environment.test_object
+
+    @property
+    def test_class(self):
+        return self._itde_cli_test_environment.test_class
+
+    @property
+    def name(self):
+        return self._itde_cli_test_environment.name
+
+    @property
+    def temp_dir(self):
+        return self._itde_cli_test_environment.temp_dir
+
+    @property
+    def repository_name(self):
+        return self._itde_cli_test_environment.repository_name
+
+    @repository_name.setter
+    def repository_name(self, value):
+        self._itde_cli_test_environment.repository_name = value
+
+    def clean_images(self):
+        self.run_command(f"{self.executable} clean-flavor-images", clean=True)
+
+    def run_command(self,
+                    command: str,
+                    use_output_directory: bool = True,
+                    use_docker_repository: bool = True,
+                    use_flavor_path: bool = True,
+                    track_task_dependencies: bool = False,
+                    clean: bool = False,
+                    capture_output: bool = False) -> CompletedProcess:
+        if use_flavor_path:
+            command = f"{command} --flavor-path {self.flavor_path} "
+        return self._itde_cli_test_environment.run_command(command=command,
+                                                           use_output_directory=use_output_directory,
+                                                           use_flavor_path=False,
+                                                           use_docker_repository=use_docker_repository,
+                                                           track_task_dependencies=track_task_dependencies,
+                                                           clean=clean,
+                                                           capture_output=capture_output)
+
+    def spawn_docker_test_environments(self, name: str, additional_parameter: List[str] = None) \
+            -> SpawnedTestEnvironments:
+        return self._itde_cli_test_environment.spawn_docker_test_environments(name, additional_parameter)
 
     def close(self):
         try:
-            if self.clean_images_at_close:
+            if self._clean_images_at_close:
                 self.clean_all_images()
         except Exception as e:
             print(e)
-        super().close()
+        self._itde_cli_test_environment.close()
 
     def clean_all_images(self):
         self.run_command(f"{self.executable} clean-all-images", use_flavor_path=False, clean=True)
@@ -55,4 +132,9 @@ def get_full_test_container_folder() -> Path:
 
 def get_mock_test_container_folder() -> Path:
     path = TEST_CONTAINER_ROOT_DIRECTORY / "full"
+    return path
+
+
+def get_test_flavor() -> Path:
+    path = FLAVORS_ROOT_DIRECTORY / "test-flavor"
     return path
