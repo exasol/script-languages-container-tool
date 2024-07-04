8c9c93d5dd653489f674ead516d7405d2e063f3e
ckunki <christoph.kuhnke@exasol.com>
2024-07-04 12:32:32 +0200

    Updated path to ODBC driver

--- exasol_script_languages_container_tool/lib/tasks/test/run_db_test.py
+++ exasol_script_languages_container_tool/lib/tasks/test/run_db_test.py
@@ -107,7 +107,7 @@ class RunDBTest(FlavorBaseTask,
         credentials = f"--user '{self.db_user}' --password '{self.db_password}'"
         log_level = f"--loglevel={self.test_log_level}"
         server = f"--server '{self._database_info.host}:{self._database_info.ports.database}'"
-        environment = "--driver=/downloads/ODBC/lib/linux/x86_64/libexaodbc-uo2214lv2.so  " \
+        environment = "--driver=/downloads/ODBC/lib/libexaodbc.so  " \
                       "--jdbc-path /downloads/JDBC/exajdbc.jar"
         language_definition = f"--script-languages '{self.language_definition}'"
         language_path = f"--lang-path /tests/lang"
