70023bbff5ddd2e03223e9828d75a7330d24394c
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-08-29 10:25:00 -0300

    Test

--- exasol_script_languages_container_tool/lib/tasks/upload/language_activation.py
+++ exasol_script_languages_container_tool/lib/tasks/upload/language_activation.py
@@ -1,8 +1,8 @@
-import urllib.parse
+from collections import namedtuple
 from dataclasses import dataclass
 from enum import Enum
 from typing import List, Union
-from urllib.parse import urlencode
+from urllib.parse import ParseResult, urlencode, urlunparse
 
 
 class SLCLanguage(Enum):
@@ -29,18 +29,51 @@ class LanguageDefinitionURL:
     language: SLCLanguage
 
     def __str__(self) -> str:
+        Components = namedtuple(  # type: ignore
+            typename="Components",
+            field_names=[
+                "scheme",
+                "netloc",
+                "url",
+                "params",
+                "query",
+                "fragment",
+            ],
+        )
+
         query_params = {p.key: v for p in self.parameters for v in p.value}
         query_params["lang"] = self.language.value.lower()
         query_string = urlencode(query_params)
         path_in_bucket = self.path_in_bucket
         if path_in_bucket and not path_in_bucket.endswith("/"):
             path_in_bucket = f"{path_in_bucket}/"
-
-        return (
-            f"{self.protocol}:///{self.bucketfs_name}/{self.bucket_name}/{path_in_bucket}"
-            f"{self.container_name}?{query_string}#buckets/{self.bucketfs_name}/{self.bucket_name}/"
-            f"{path_in_bucket}{self.container_name}{self.udf_client_path_within_container}"
+        # this does not work:cannot instantiate ParseResult
+        # url = urlunparse(
+        #     ParseResult(
+        #         fragment=f"buckets/{self.bucketfs_name}/{self.bucket_name}/{path_in_bucket}",
+        #         hostname=None,
+        #         netloc="",
+        #         params="",
+        #         password=None,
+        #         path=f"/{self.bucketfs_name}/{self.bucket_name}/{self.path_in_bucket}/{self.container_name}",
+        #         port=None,
+        #         query=query_string,
+        #         scheme=self.protocol,
+        #         username=None,
+        #     )
+        # )
+        url = urlunparse(
+            Components(
+                scheme=self.protocol,  # type: ignore
+                netloc="",
+                query=query_string,
+                url=f"///{self.bucketfs_name}/{self.bucket_name}/{path_in_bucket}{self.container_name}",
+                params="",
+                fragment=f"buckets/{self.bucketfs_name}/{self.bucket_name}/{path_in_bucket}"
+                f"{self.container_name}{self.udf_client_path_within_container}",
+            )
         )
+        return url  # type: ignore
 
 
 @dataclass
