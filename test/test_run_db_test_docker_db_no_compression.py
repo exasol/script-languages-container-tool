83b434caf2a9542de7c4cfa7457fda45dcd15202
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-04-03 17:56:51 -0300

    Added tests

---
+++ test/test_run_db_test_docker_db_no_compression.py
@@ -0,0 +1,40 @@
+import unittest
+
+import utils as exaslct_utils  # type: ignore # pylint: disable=import-error
+from exasol_integration_test_docker_environment.lib.docker.container.utils import (
+    remove_docker_container,
+)
+
+
+class DockerRunDBTestDockerDBTestNoCompression(unittest.TestCase):
+
+    def setUp(self):
+        print(f"SetUp {self.__class__.__name__}")
+        self.test_environment = exaslct_utils.ExaslctTestEnvironmentWithCleanUp(
+            self, exaslct_utils.EXASLCT_DEFAULT_BIN
+        )
+        self.test_environment.clean_images()
+
+    def tearDown(self):
+        self.remove_docker_container()
+        self.test_environment.close()
+
+    def remove_docker_container(self):
+        remove_docker_container(
+            [
+                f"test_container_{self.test_environment.name}",
+                f"db_container_{self.test_environment.name}",
+            ]
+        )
+
+    def test_run_db_tests_docker_db(self):
+        command = (
+            f"{self.test_environment.executable} run-db-test "
+            f"{exaslct_utils.get_full_test_container_folder_parameter()}"
+            "--no-compression"
+        )
+        self.test_environment.run_command(command, track_task_dependencies=True)
+
+
+if __name__ == "__main__":
+    unittest.main()
