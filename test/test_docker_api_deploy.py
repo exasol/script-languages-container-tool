de128604b5e019b72c11ce84c9358d88b261f790
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-04-07 08:28:28 -0300

    Unified tests in test_docker_api_deploy.py

--- test/test_docker_api_deploy.py
+++ test/test_docker_api_deploy.py
@@ -1,6 +1,7 @@
 import subprocess
 import tarfile
 import unittest
+from functools import partial
 from tempfile import TemporaryDirectory
 
 import exasol.bucketfs as bfs  # type: ignore
@@ -29,13 +30,13 @@ class ApiDockerDeployTest(unittest.TestCase):
     def tearDown(self):
         utils.close_environments(self.test_environment, self.docker_environment)
 
-    def _validate_deploy(self, compression: bool, expected_extension: str):
-        path_in_bucket = "test"
+    def _validate_deploy(self, compression: bool, path: str, expected_extension: str):
         release_name = "TEST"
         bucketfs_name = "bfsdefault"
         bucket_name = "default"
         flavor_path = exaslct_utils.get_test_flavor()
-        result = api.deploy(
+        deploy_func = partial(
+            api.deploy,
             flavor_path=(str(flavor_path),),
             bucketfs_host=self.docker_environment.database_host,
             bucketfs_port=self.docker_environment.ports.bucketfs,
@@ -45,22 +46,37 @@ class ApiDockerDeployTest(unittest.TestCase):
             bucketfs_use_https=False,
             bucketfs_name=bucketfs_name,
             bucket=bucket_name,
-            path_in_bucket=path_in_bucket,
             release_name=release_name,
             compression=compression,
         )
+        if path:
+            result = deploy_func(path_in_bucket=path)
+        else:
+            result = deploy_func()
+
         self.assertIn(str(flavor_path), result.keys())
 
         self.assertEqual(len(result), 1)
         self.assertIn(str(flavor_path), result.keys())
         self.assertEqual(len(result[str(flavor_path)]), 1)
         deploy_result = result[str(flavor_path)]["release"]
-        expected_alter_session_cmd = (
-            f"ALTER SESSION SET SCRIPT_LANGUAGES='PYTHON3_TEST=localzmq+protobuf:///{bucketfs_name}/"
-            f"{bucket_name}/{path_in_bucket}/test-flavor-release-{release_name}?lang=python#buckets/"
-            f"{bucketfs_name}/{bucket_name}/{path_in_bucket}/test-flavor-release-{release_name}/"
-            f"exaudf/exaudfclient_py3';"
+
+        complete_path_in_bucket = "/".join(
+            filter(
+                None,
+                [
+                    bucketfs_name,
+                    bucket_name,
+                    path,
+                    f"test-flavor-release-{release_name}",
+                ],
+            )
         )
+        expected_alter_session_cmd = (
+            f"ALTER SESSION SET SCRIPT_LANGUAGES='PYTHON3_TEST=localzmq+protobuf:///{complete_path_in_bucket}?lang=python#buckets/"
+            f"{complete_path_in_bucket}/exaudf/exaudfclient_py3';"
+        )
+
         result_alter_session_cmd = (
             deploy_result.language_definition_builder.generate_alter_session()
         )
@@ -72,26 +88,43 @@ class ApiDockerDeployTest(unittest.TestCase):
             deploy_result.release_path,
         )
 
+        upload_path = "/".join(
+            filter(
+                None,
+                [
+                    bucket_name,
+                    path,
+                    f"test-flavor-release-{release_name}{expected_extension}",
+                ],
+            )
+        )
         self.assertEqual(
             deploy_result.human_readable_upload_location,
             f"http://{self.docker_environment.database_host}:{self.docker_environment.ports.bucketfs}/"
-            f"{bucket_name}/{path_in_bucket}/test-flavor-release-{release_name}{expected_extension}",
+            f"{upload_path}",
         )
 
-        expected_path_in_bucket = (
-            bfs.path.build_path(
-                backend=bfs.path.StorageBackend.onprem,
-                url=f"http://{self.docker_environment.database_host}:{self.docker_environment.ports.bucketfs}",
-                bucket_name=bucket_name,
-                service_name=bucketfs_name,
-                username="w",
-                password=self.docker_environment.bucketfs_password,
-                verify=False,
-                path=path_in_bucket,
+        build_path_func = partial(
+            bfs.path.build_path,
+            backend=bfs.path.StorageBackend.onprem,
+            url=f"http://{self.docker_environment.database_host}:{self.docker_environment.ports.bucketfs}",
+            bucket_name=bucket_name,
+            service_name=bucketfs_name,
+            username="w",
+            password=self.docker_environment.bucketfs_password,
+            verify=False,
+        )
+
+        if path:
+            expected_path_in_bucket = (
+                build_path_func(path=path)
+                / f"test-flavor-release-{release_name}{expected_extension}"
             )
-            / f"test-flavor-release-{release_name}{expected_extension}"
-        )
-
+        else:
+            expected_path_in_bucket = (
+                build_path_func()
+                / f"test-flavor-release-{release_name}{expected_extension}"
+            )
         # Compare UDF path of `bucket_path` until bfs.path.PathLike implements comparison
         self.assertEqual(
             expected_path_in_bucket.as_udf_path(),
@@ -100,85 +133,22 @@ class ApiDockerDeployTest(unittest.TestCase):
 
         self.validate_file_on_bucket_fs(
             bucket_name,
-            path_in_bucket,
+            path,
             f"test-flavor-release-{release_name}{expected_extension}",
             compression=compression,
         )
 
     def test_docker_api_deploy(self):
-        self._validate_deploy(compression=True, expected_extension=".tar.gz")
+        self._validate_deploy(
+            compression=True, path="test", expected_extension=".tar.gz"
+        )
 
     def test_docker_api_deploy_no_compression(self):
-        self._validate_deploy(compression=False, expected_extension=".tar")
+        self._validate_deploy(compression=False, path="test", expected_extension=".tar")
 
     def test_docker_api_deploy_without_path_in_bucket(self):
-        release_name = "TEST"
-        bucketfs_name = "bfsdefault"
-        bucket_name = "default"
-        flavor_path = exaslct_utils.get_test_flavor()
-        result = api.deploy(
-            flavor_path=(str(flavor_path),),
-            bucketfs_host=self.docker_environment.database_host,
-            bucketfs_port=self.docker_environment.ports.bucketfs,
-            bucketfs_user=self.docker_environment.bucketfs_username,
-            bucketfs_password=self.docker_environment.bucketfs_password,
-            target_docker_repository_name=self.test_environment.docker_repository_name,
-            bucketfs_use_https=False,
-            bucketfs_name=bucketfs_name,
-            bucket=bucket_name,
-            release_name=release_name,
-        )
-        self.assertIn(str(flavor_path), result.keys())
-        self.assertEqual(len(result), 1)
-        self.assertIn(str(flavor_path), result.keys())
-        self.assertEqual(len(result[str(flavor_path)]), 1)
-        deploy_result = result[str(flavor_path)]["release"]
-        expected_alter_session_cmd = (
-            f"ALTER SESSION SET SCRIPT_LANGUAGES='PYTHON3_TEST=localzmq+protobuf:///{bucketfs_name}/"
-            f"{bucket_name}/test-flavor-release-{release_name}?lang=python#buckets/"
-            f"{bucketfs_name}/{bucket_name}/test-flavor-release-{release_name}/"
-            f"exaudf/exaudfclient_py3';"
-        )
-        result_alter_session_cmd = (
-            deploy_result.language_definition_builder.generate_alter_session()
-        )
-        self.assertEqual(result_alter_session_cmd, expected_alter_session_cmd)
-
-        self.assertIn(
-            f".build_output/cache/exports/test-flavor-release-",
-            deploy_result.release_path,
-        )
-
-        self.assertEqual(
-            deploy_result.human_readable_upload_location,
-            f"http://{self.docker_environment.database_host}:{self.docker_environment.ports.bucketfs}/"
-            f"{bucket_name}/test-flavor-release-{release_name}.tar.gz",
-        )
-
-        expected_path_in_bucket = (
-            bfs.path.build_path(
-                backend=bfs.path.StorageBackend.onprem,
-                url=f"http://{self.docker_environment.database_host}:{self.docker_environment.ports.bucketfs}",
-                bucket_name=bucket_name,
-                service_name=bucketfs_name,
-                username="w",
-                password=self.docker_environment.bucketfs_password,
-                verify=False,
-            )
-            / f"test-flavor-release-{release_name}.tar.gz"
-        )
-
-        # Compare UDF path of `bucket_path` until bfs.path.PathLike implements comparison
-        self.assertEqual(
-            expected_path_in_bucket.as_udf_path(),
-            deploy_result.bucket_path.as_udf_path(),
-        )
-
-        self.validate_file_on_bucket_fs(
-            bucket_name,
-            "",
-            f"test-flavor-release-{release_name}.tar.gz",
-            compression=True,
+        self._validate_deploy(
+            compression=True, path="test", expected_extension=".tar.gz"
         )
 
     def test_docker_api_deploy_fail_path_in_bucket(self):
@@ -201,36 +171,6 @@ class ApiDockerDeployTest(unittest.TestCase):
         except TaskRuntimeError:
             exception_thrown = True
         assert exception_thrown
-
-    # def check_file_in_bucketfs(
-    #     self,
-    #     url: str,
-    #     bucket_name: str,
-    #     bucketfs_username: str,
-    #     bucketfs_password: str,
-    #     path: str,
-    #     expected_file: str,
-    #     compression: bool,
-    # ):
-    #     CREDENTIALS = {
-    #         bucket_name: {"username": bucketfs_username, "password": bucketfs_password}
-    #     }
-    #
-    #     bucketfs = Service(url, CREDENTIALS)
-    #     bucket = bucketfs[bucket_name]
-    #
-    #     with TemporaryDirectory() as tmpdir:
-    #         file = as_file(
-    #             bucket.download(f"{path}/{expected_file}"),
-    #             filename=Path(tmpdir) / expected_file,
-    #         )
-    #
-    #         tar_mode = "r:gz" if compression else "r:"
-    #         with tarfile.open(name=file, mode=tar_mode) as tf:
-    #             tf_members = tf.getmembers()
-    #             last_tf_member = tf_members[-1]
-    #             assert last_tf_member.name == "exasol-manifest.json"
-    #             assert last_tf_member.path == "exasol-manifest.json"
 
     def check_file_in_bucketfs(
         self,
