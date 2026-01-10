d45896d1ab279c8637c6279b8cc922abfa140f76
Torsten Kilias <tkilias@users.noreply.github.com>
2026-01-10 11:30:51 +0100

    Add default values to optional parameter in UploadContainerParameter

--- exasol/slc/internal/tasks/upload/upload_container_parameter.py
+++ exasol/slc/internal/tasks/upload/upload_container_parameter.py
@@ -8,21 +8,21 @@ from exasol.slc.internal.tasks.export.export_container_parameters import (
 
 
 class UploadContainerParameter(ExportContainerOptionsParameter):
-    database_host: str = luigi.OptionalParameter()  # type: ignore
-    bucketfs_port: int = luigi.OptionalParameter()  # type: ignore
-    bucketfs_username: str = luigi.OptionalParameter(significant=False)  # type: ignore
+    database_host: str = luigi.OptionalParameter(None)  # type: ignore
+    bucketfs_port: int = luigi.OptionalParameter(None)  # type: ignore
+    bucketfs_username: str = luigi.OptionalParameter(None, significant=False)  # type: ignore
     bucketfs_password: str = luigi.OptionalParameter(
-        significant=False, visibility=luigi.parameter.ParameterVisibility.HIDDEN
+        None, significant=False, visibility=luigi.parameter.ParameterVisibility.HIDDEN
     )  # type: ignore
-    bucketfs_name: str = luigi.OptionalParameter()  # type: ignore
-    bucket_name: str = luigi.OptionalParameter()  # type: ignore
-    path_in_bucket: Optional[str] = luigi.OptionalParameter()  # type: ignore
+    bucketfs_name: str = luigi.OptionalParameter(None)  # type: ignore
+    bucket_name: str = luigi.OptionalParameter(None)  # type: ignore
+path_in_bucket: Optional[str] = luigi.OptionalParameter(None)  # type: ignore
     bucketfs_https: bool = luigi.BoolParameter(False)  # type: ignore
-    release_name: Optional[str] = luigi.OptionalParameter()  # type: ignore
-    ssl_cert_path: str = luigi.OptionalParameter()  # type: ignore
+    release_name: Optional[str] = luigi.OptionalParameter(None)  # type: ignore
+    ssl_cert_path: str = luigi.OptionalParameter(None)  # type: ignore
     use_ssl_cert_validation: bool = luigi.BoolParameter(True)  # type: ignore
-    saas_host: str = luigi.OptionalParameter()  # type: ignore
-    saas_pat: str = luigi.OptionalParameter()  # type: ignore
-    saas_account_id: str = luigi.OptionalParameter()  # type: ignore
-    saas_database_id: str = luigi.OptionalParameter()  # type: ignore
-    saas_database_name: str = luigi.OptionalParameter()  # type: ignore
+    saas_host: str = luigi.OptionalParameter(None)  # type: ignore
+    saas_pat: str = luigi.OptionalParameter(None)  # type: ignore
+    saas_account_id: str = luigi.OptionalParameter(None)  # type: ignore
+    saas_database_id: str = luigi.OptionalParameter(None)  # type: ignore
+    saas_database_name: str = luigi.OptionalParameter(None)  # type: ignore
