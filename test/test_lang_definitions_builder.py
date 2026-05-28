dba6caa750721f6a8b0e722b5f7ed56ef567eaac
Torsten Kilias <torsten.kilias@exasol.com>
2026-05-28 09:57:23 +0200

    Fix build_name expectations in integration tests

--- test/test_lang_definitions_builder.py
+++ test/test_lang_definitions_builder.py
@@ -249,8 +249,8 @@ class LanguageDefinitionBuilderTest(unittest.TestCase):
             self.assertEqual(
                 alter_system,
                 "ALTER SYSTEM SET SCRIPT_LANGUAGES='MY_PYTHON3="
-                "localzmq+protobuf:///bfsdefault/default/some_path/my_release"
-                "?lang=python&my_param=hello#buckets/bfsdefault/default/some_path/my_release"
+                "localzmq+protobuf:///bfsdefault/default/some_path/my_build"
+                "?lang=python&my_param=hello#buckets/bfsdefault/default/some_path/my_build"
                 "/exaudf/exaudfclient_py3 JAVA=builtin_java "
                 "PYTHON3=builtin_python3 R=builtin_r';",
             )
