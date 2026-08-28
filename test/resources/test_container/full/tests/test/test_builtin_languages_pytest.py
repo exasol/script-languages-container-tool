4bad3cbd0b0099306623d2b2cbe4b9976fabfbf9
Steffen Pankratz <steffen.pankratz@exasol.com>
2026-08-28 16:12:49 +0200

    WIP
    Signed-off-by: Steffen Pankratz <steffen.pankratz@exasol.com>

--- test/resources/test_container/full/tests/test/test_builtin_languages_pytest.py
+++ test/resources/test_container/full/tests/test/test_builtin_languages_pytest.py
@@ -1,5 +1,23 @@
 import textwrap
 
+from exasol.python_extension_common.deployment.language_container_deployer import (
+    LanguageActivationLevel,
+    get_language_settings,
+)
+
+EXPECTED_SCRIPT_LANGUAGES = (
+    "PYTHON3_TEST=localzmq+protobuf:///bfsdefault/myudfs/test-flavor?lang=python"
+    "#buckets/bfsdefault/myudfs/test-flavor/exaudf/exaudfclient_py3 "
+    "JAVA=builtin_java PYTHON3=builtin_python3 R=builtin_r"
+)
+
+
+def _assert_script_languages(connection, expected_script_languages):
+    actual_script_languages = get_language_settings(
+        connection, LanguageActivationLevel.Session
+    )
+    assert actual_script_languages == expected_script_languages
+
 
 def _execute_script(connection, language, script_name, script_body):
     script_body = textwrap.dedent(script_body).strip()
@@ -13,6 +31,7 @@ def _execute_script(connection, language, script_name, script_body):
 
 def test_python3(pyexasol_connection, activate_script_languages_for_function):
     try:
+        _assert_script_languages(pyexasol_connection, EXPECTED_SCRIPT_LANGUAGES)
         _execute_script(
             pyexasol_connection,
             "PYTHON3",
@@ -28,6 +47,7 @@ def test_python3(pyexasol_connection, activate_script_languages_for_function):
 
 def test_java(pyexasol_connection, activate_script_languages_for_function):
     try:
+        _assert_script_languages(pyexasol_connection, EXPECTED_SCRIPT_LANGUAGES)
         _execute_script(
             pyexasol_connection,
             "JAVA",
@@ -46,6 +66,7 @@ def test_java(pyexasol_connection, activate_script_languages_for_function):
 
 def test_r(pyexasol_connection, activate_script_languages_for_function):
     try:
+        _assert_script_languages(pyexasol_connection, EXPECTED_SCRIPT_LANGUAGES)
         _execute_script(
             pyexasol_connection,
             "R",
